2fa4bd47434c05180b4293f7237d9e0c588fbbd6
rajeshd-ezdi <rajesh.d@ezdi.us>
2017-05-25 18:55:12 +0530

    kkk

---
+++ src/main/java/ezdi/product1/m1/App.java
@@ -0,0 +1,13 @@
+package ezdi.product1.m1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Rajesh" );
+    }
+}
